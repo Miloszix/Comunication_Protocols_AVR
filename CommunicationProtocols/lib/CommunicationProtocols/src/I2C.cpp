f80ed7bf040a0758833a7393465d56695f1b8575
Miłosz M <miloszmynarczuk@gmail.com>
2025-01-05 17:36:57 +0100

    i2c corection

--- CommunicationProtocols/lib/CommunicationProtocols/src/I2C.cpp
+++ CommunicationProtocols/lib/CommunicationProtocols/src/I2C.cpp
@@ -2,129 +2,107 @@
 #include "I2C.h"
 #include <avr/io.h>
 
-#define TWBR_VALUE 72  // Wartość TWBR dla żądanej prędkości (100 kHz przy F_CPU = 16 MHz)
+#define TWBR_VALUE 72
 
 void I2C::init() {
-    // Ustawienie preskalera TWI
-    TWSR = 0x00;  // Brak preskalera (preskaler = 1)
-
-    // Ustawienie wartości TWBR (TWI Bit Rate Register)
+    TWSR = 0x00;
     TWBR = TWBR_VALUE;
-
-    // Włączenie TWI (Two-Wire Interface)
-    TWCR = (1 << TWEN);  // Włącz TWI i pozwól na generowanie przerwań
+    TWCR = (1 << TWEN);
 }
 
 // Funkcja do wysyłania danych przez I2C
 void I2C::start() {
-    // Ustawienie bitu TWINT w TWCR, aby rozpocząć transmisję
     TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-    
-    // Czekaj, aż bit TWINT będzie ustawiony
-    while (!(TWCR & (1 << TWINT)));
-}
-
-// Funkcja do wysyłania bajtu przez I2C
-void I2C::write(uint8_t data) {
-    TWDR = data;  // Załaduj dane do rejestru TWI Data Register
-    TWCR = (1 << TWINT) | (1 << TWEN);  // Rozpocznij transmisję
-    
-    // Czekaj, aż transmisja zostanie zakończona
     while (!(TWCR & (1 << TWINT)));
 }
 
 // Funkcja do zakończenia transmisji I2C
 void I2C::stop() {
-    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);  // Ustaw bit TWSTO i TWINT, aby zakończyć transmisję
-    while (TWCR & (1 << TWSTO));  // Czekaj na zakończenie transmisji
+    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+    while (TWCR & (1 << TWSTO));
 }
 
+// Funkcja do wysyłania bajtu przez I2C
+void I2C::write(uint8_t data) {
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+}
+
+// Funkcja do zapisu pojedynczego bajtu do urządzenia I2C bez podawania rejestru
+void I2C::write_data(uint8_t slave_address, uint8_t data) {
+    I2C::start();
+    I2C::write(slave_address << 1);
+    I2C::write(data);   
+    I2C::stop();
+}
 
 // Funkcja do zapisu pojedynczego rejestru w urządzeniu I2C
 void I2C::write_register(uint8_t slave_address, uint8_t reg, uint8_t data) {
     I2C::start();
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem zapisu - 0)
     I2C::write(slave_address << 1);
-    
-    // Wysłanie adresu rejestru
     I2C::write(reg);
-    
-    // Wysłanie danych do rejestru
     I2C::write(data);
-    
     I2C::stop();
+}
+
+// Funkcja do odbierania danych (w trybie Master)
+uint8_t I2C::read(bool ack) {
+    TWCR = (1 << TWINT) | (ack ? (1 << TWEA) : 0) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+    return TWDR;
+}
+
+// Funkcja do odczytu pojedynczego bajtu z urządzenia I2C bez podawania rejestru
+uint8_t I2C::read_data(uint8_t slave_address) {
+    uint8_t data;
+    I2C::start();
+    I2C::write(slave_address << 1);
+    I2C::start();
+    I2C::write((slave_address << 1) | 1);
+    data = I2C::read(false);
+    I2C::stop();
+    return data;
 }
 
 // Funkcja do odczytu pojedynczego rejestru z urządzenia I2C
 uint8_t I2C::read_register(uint8_t slave_address, uint8_t reg) {
     uint8_t data;
-    
     I2C::start();
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem zapisu - 0)
     I2C::write(slave_address << 1);
-    
-    // Wysłanie adresu rejestru
     I2C::write(reg);
-    
-    I2C::start();  // Ponowne wystartowanie transmisji dla odczytu
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem odczytu - 1)
+    I2C::start();
     I2C::write((slave_address << 1) | 1);
-    
-    // Odczyt danych z urządzenia I2C
-    data = I2C::read(false);  // Odczyt bez ustawiania ACK (koniec transmisji)
-    
+    data = I2C::read(false);
     I2C::stop();
-    
     return data;
 }
 
-// Funkcja do odbierania danych (w trybie Master)
-uint8_t I2C::read(bool ack) {
-    TWCR = (1 << TWINT) | (ack ? (1 << TWEA) : 0) | (1 << TWEN);  // Ustawienie ACK lub NACK
-    while (!(TWCR & (1 << TWINT)));  // Czekaj na zakończenie
-    return TWDR;  // Zwróć odebrane dane
-}
 // Funkcja do zapisu bloku danych do urządzenia I2C
 void I2C::write_block(uint8_t slave_address, uint8_t reg, uint8_t* data, uint8_t size) {
     I2C::start();
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem zapisu - 0)
     I2C::write(slave_address << 1);
-    
-    // Wysłanie adresu rejestru
     I2C::write(reg);
-    
-    // Wysłanie danych (blok)
+
     for (uint8_t i = 0; i < size; i++) {
         I2C::write(data[i]);
     }
-    
+
     I2C::stop();
 }
 
 // Funkcja do odczytu bloku danych z urządzenia I2C
 void I2C::read_block(uint8_t slave_address, uint8_t reg, uint8_t* data, uint8_t size) {
     I2C::start();
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem zapisu - 0)
     I2C::write(slave_address << 1);
-    
-    // Wysłanie adresu rejestru
     I2C::write(reg);
-    
-    I2C::start();  // Ponowne wystartowanie transmisji dla odczytu
-    
-    // Wysłanie adresu urządzenia I2C (z ustawionym bitem odczytu - 1)
+    I2C::start();
     I2C::write((slave_address << 1) | 1);
-    
-    // Odczyt danych z urządzenia I2C (blok danych)
+
     for (uint8_t i = 0; i < size - 1; i++) {
-        data[i] = I2C::read(true);  // Odczyt z ACK dla każdego bajtu
+        data[i] = I2C::read(true);
     }
-    data[size - 1] = I2C::read(false);  // Odczyt z NACK dla ostatniego bajtu
-    
+
+    data[size - 1] = I2C::read(false);
     I2C::stop();
 }
--- CommunicationProtocols/lib/CommunicationProtocols/src/I2C.h
+++ CommunicationProtocols/lib/CommunicationProtocols/src/I2C.h
@@ -8,44 +8,32 @@ class I2C : public CommunicationProtocol {
 private:
     uint32_t bitrate;      // Prędkość transmisji I2C
     uint8_t prescaler;     // Przeskalowanie zegara TWI
-    uint8_t slaveAddress;  // Adres urządzenia I2C
-    bool isMaster;         // Czy tryb Master (true) czy Slave (false)
 
 public:
+
+    //napisać konstruktor
+
     void init() override;
     void start() override;
     void stop() override;
     void write(uint8_t data);
+    void write_data(uint8_t slave_address, uint8_t data);
     void write_register(uint8_t slave_address, uint8_t reg, uint8_t data);
-    uint8_t read_register(uint8_t slaveAddress, uint8_t reg);
     uint8_t read(bool ack);
+    uint8_t read_data(uint8_t slave_address);
+    uint8_t read_register(uint8_t slave_Address, uint8_t reg);
     void write_block(uint8_t slave_address, uint8_t reg, uint8_t* data, uint8_t size);
     void read_block(uint8_t slave_address, uint8_t reg, uint8_t* data, uint8_t size);
     
-
     void setBitrate(uint32_t b) {
         bitrate = b;
-        init();  // Zainicjuj ponownie po zmianie bitrate
+        init();
     }
 
     // Setter dla preskalera
     void setPrescaler(uint8_t p) {
         prescaler = p;
-        init();  // Zainicjuj ponownie po zmianie preskalera
-    }
-
-    // Setter dla adresu Slave
-    void setSlaveAddress(uint8_t address) {
-        slaveAddress = address;
-        if (!isMaster) {
-            TWAR = (slaveAddress << 1);  // Ustaw adres Slave
-        }
-    }
-
-    // Setter dla trybu Master/Slave
-    void setMode(bool master) {
-        isMaster = master;
-        init();  // Zainicjuj ponownie po zmianie trybu
+        init();
     }
 
     uint32_t getBitrate() const {
@@ -55,16 +43,6 @@ public:
     // Getter dla preskalera
     uint8_t getPrescaler() const {
         return prescaler;
-    }
-
-    // Getter dla adresu Slave
-    uint8_t getSlaveAddress() const {
-        return slaveAddress;
-    }
-
-    // Getter dla trybu Master/Slave
-    bool getMode() const {
-        return isMaster;
     }
 };
 
